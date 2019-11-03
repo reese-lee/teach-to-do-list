3016dbeae71ff527a6ec309e61d3c5af9485f8e2
Reese Lee <reese.lee.cy@gmail.com>
2019-11-03 14:24:40 -0800

    add code to display current date

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,33 @@ Task.prototype.markDone = function(id) {
 // UI logic 
 let toDoList = new ToDoList();
 
+function displayListDetails(newListToDisplay) {
+    let tasksList = $("ul#tasks");
+    let htmlForTasksInfo = "";
+    newListToDisplay.tasks.forEach(function(task) {
+        htmlForTasksInfo += "<li id=" + task.id + ">" + task.name + "</li>"
+    });
+    tasksList.html(htmlForTasksInfo);
+    }
+
+$(document).ready(function() {
+    $("form#new-task").submit(function(event){
+        event.preventDefault();
+        let inputtedTaskName = $("input#new-task").val();
+
+        let newTask = new Task(inputtedTaskName, isDone);
+        toDoList.addTask(newTask);
+        displayListDetails(toDoList);
+    });
+
+    let d = new Date();
+    let month = d.getMonth()+1;
+    let day = d.getDate();
+
+    let output = d.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day;
+        console.log(output);
+    
+    $("#currentDate").html(output);
+})
